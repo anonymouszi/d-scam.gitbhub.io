07f9cc17df8e16d41a261eab6d87a7593faff62a
Ian Christopher B. de Jesus <3683356+iandjx@users.noreply.github.com>
2023-05-20 08:56:57 +0800

    fix: add seo

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -2,14 +2,15 @@ import type { GatsbyConfig } from "gatsby";
 
 const config: GatsbyConfig = {
   siteMetadata: {
-    title: `Driver's License Scammers`,
-    siteUrl: `https://www.yourdomain.tld`
+    title: `Driver's License Fixers Scammers in Philippines`,
+    siteUrl: `https://anonymouszi.github.io/d-scam.gitbhub.io/`,
+    description: `List of existing driver's license scammers and fixers in Philippiines found on Facebook Marketplace.`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-postcss"]
+  plugins: ["gatsby-plugin-postcss", "gatsby-plugin-react-helmet"],
 };
 
 export default config;
